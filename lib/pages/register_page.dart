580d671919b4e19346efe8a195248a376a1b8021
Sorochan Andriy <xai1983kbu@gmail.com>
2019-05-10 15:19:53 +0300

    Adding Form Validation, Creating FormState

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -6,67 +6,84 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _username, _email, _password;
+
   Widget _showTitle() {
     return Text('Register', style: Theme.of(context).textTheme.headline);
   }
 
   Widget _showUsernameInput() {
     return Padding(padding: EdgeInsets.only(top: 20.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Username',
-                      hintText: 'Enter username, min length 6',
-                      icon: Icon(Icons.face, color: Colors.grey,)
-                    ),
-                  ),);
+      child: TextFormField(
+        onSaved: (val) => _username = val,
+        validator: (val) => val.length < 6 ? 'Username too short' : null,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Username',
+          hintText: 'Enter username, min length 6',
+          icon: Icon(Icons.face, color: Colors.grey,)
+        ),
+      ),);
   }
 
   Widget _showEmailInput() {
     return Padding(padding: EdgeInsets.only(top: 20.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                      hintText: 'Enter a valid email',
-                      icon: Icon(Icons.mail, color: Colors.grey,)
-                    ),
-                  ),);
+      child: TextFormField(
+        onSaved: (val) => _username = val,
+        validator: (val) => !val.contains('@') ? 'Invalid Email' : null,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Email',
+          hintText: 'Enter a valid email',
+          icon: Icon(Icons.mail, color: Colors.grey,)
+        ),
+      ),);
   }
 
   Widget _showPasswordInput() {
     return Padding(padding: EdgeInsets.only(top: 20.0),
-                  child: TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      hintText: 'Enter password, min length 6',
-                      icon: Icon(Icons.lock, color: Colors.grey,)
-                    ),
-                  ),);
+      child: TextFormField(
+        onSaved: (val) => _username = val,
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Password',
+          hintText: 'Enter password, min length 6',
+          icon: Icon(Icons.lock, color: Colors.grey,)
+        ),
+      ),);
   }
 
   Widget _showFormActions() {
     return Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: Column(
-                      children: <Widget>[
-                        RaisedButton(
-                          child: Text(
-                            'Submit', 
-                            style: Theme.of(context).textTheme.body1.copyWith(color: Colors.black),),
-                          elevation: 8.0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10.0))
-                          ),
-                          color: Theme.of(context).primaryColor,
-                          onPressed: () => print('submit'),
-                        ),
-                        FlatButton(
-                          child: Text('Existing user? Login'),
-                          onPressed: () => print('login'),
-                        );
+      padding: EdgeInsets.only(top: 20.0),
+      child: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Text(
+              'Submit', 
+              style: Theme.of(context).textTheme.body1.copyWith(color: Colors.black),),
+            elevation: 8.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0))
+            ),
+            color: Theme.of(context).primaryColor,
+            onPressed: _submit
+          ),
+          FlatButton(
+            child: Text('Existing user? Login'),
+            onPressed: () => print('login'),),],),);
+  }
+
+  void _submit() {
+    final form = _formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+      print('Username: $_username, Email: $_email, Password: $_password');
+    }
   }
 
   @override
@@ -80,6 +97,7 @@ class _RegisterPageState extends State<RegisterPage> {
         child: Center(
           child: SingleChildScrollView(
             child: Form(
+              key: _formKey,
               child: Column(
                 children: <Widget>[
                   _showTitle(),
@@ -90,12 +108,9 @@ class _RegisterPageState extends State<RegisterPage> {
                   ],
 
                   ),)
-                ],
               ),
             ),
           ),
-        ),
-      ),
     );
   }
 }
