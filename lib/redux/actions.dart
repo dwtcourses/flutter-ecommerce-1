804b0104cef0e1a15e52be926b16b428c6d27dd4
Sorochan Andriy <xai1983kbu@gmail.com>
2019-05-20 19:51:17 +0300

    Displaying Processed Orders, Formatting Order Time

--- lib/redux/actions.dart
+++ lib/redux/actions.dart
@@ -178,9 +178,23 @@ ThunkAction<AppState> getCardTokenAction = (Store<AppState> store) async {
     'Authorization': 'Bearer $jwt'
   });
   final responseData = json.decode(response.body);
+  List<Order> orders = [];
+  responseData['orders'].forEach((orderData){
+    final Order order = Order.fromJson(orderData);
+    orders.add(order);
+  });
   final cardToken = responseData['card_token'];
   store.dispatch(GetCardTokenAction(cardToken));
+  store.dispatch(GetOrdersAction(orders));
 };
+
+class GetOrdersAction {
+  final List<Order> _orders;
+
+  List<Order> get orders => this._orders;
+
+  GetOrdersAction(this._orders);
+}
 
 class UpdateCardAction {
   final String _cardToken;
